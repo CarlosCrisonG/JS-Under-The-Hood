9d915438661bf3df3c17903b5806b0c719802f35
CarlosCrisonG <carloscrison@gmail.com>
2023-07-24 03:53:00 -0500

    Faking namespaces

---
+++ D3_FakingNamespaces/Starter/app.js
@@ -0,0 +1,13 @@
+var greet = 'Hello!';
+var greet = 'Hola!'; 
+
+console.log(greet);
+
+var english = {
+    greetings: {
+        basic: 'Hello!'
+    }
+};
+var spanish =  {};
+
+console.log(english.greetings.basic);
